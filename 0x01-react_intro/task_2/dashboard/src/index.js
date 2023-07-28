32fa2f611a42486a61187a073da7ddfa2e52ae0c
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-07-28 05:45:49 -0700

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -8,15 +8,10 @@ import Notifications from './Notifications';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Notifications />
     <App />
   </React.StrictMode>
 );
-// const rootNotifications = ReactDOM.createRoot(document.getElementById('root'));
-// rootNotifications.render(
-//   <React.StrictMode>
-//     <Notifications />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
