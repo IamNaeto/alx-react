4eb1d99c3e83475f1ffe782589ae444a98b706b8
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-08-09 00:03:47 +0000

    Conditionally applying style

--- 0x04-React_inline_styling/task_2/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,27 +1,34 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import BodySection from "./BodySection";
-import { StyleSheet, css } from "aphrodite";
+import React, { Component } from 'react'
+import BodySection from './BodySection';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 class BodySectionWithMarginBottom extends Component {
   render() {
     return (
-      <div className={css(styles.bodySectionWithMargin)}>
-        <BodySection {...this.props} />
+      <div className={css(bodySection.bodySectionWithMargin)}>
+        <BodySection {...this.props}/>
       </div>
     );
   }
-}
+};
 
-const styles = StyleSheet.create({
-  bodySectionWithMargin: {
-    marginBottom: "40",
-  },
+const bodySection = StyleSheet.create({
+  bodySectionWithMargin : {
+    marginBottom: '40px'
+  }
 });
+
+BodySectionWithMarginBottom. defaultProps = {
+	children: <React.Fragment />
+};
 
 BodySectionWithMarginBottom.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element
+  ])
 };
 
 export default BodySectionWithMarginBottom;
--- 0x04-React_inline_styling/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,19 +1,21 @@
-import React, { Component } from "react";
-import { StyleSheet, css } from "aphrodite";
+import React from "react";
+import "./Notifications.css";
 import closeIcon from "../assets/close-icon.png";
 import NotificationItem from "./NotificationItem";
-import PropTypes from "prop-types";
+import PropeTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
+import { StyleSheet, css } from "aphrodite";
 
-class Notifications extends Component {
+class Notifications extends React.Component {
   constructor(props) {
     super(props);
-
     this.markAsRead = this.markAsRead.bind(this);
   }
 
   shouldComponentUpdate(nextProps) {
-    return nextProps.length > this.props.listNotifications.length;
+    return (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    );
   }
 
   markAsRead(id) {
@@ -22,12 +24,10 @@ class Notifications extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <div className={css(styles.menuItem)}>
-          <p>Your notifications</p>
-        </div>
+      <>
+        <div className={css(notificationStyles.menuItem)}>Your notifications</div>
         {this.props.displayDrawer ? (
-          <div className={css(styles.Notifications)}>
+          <div className={css(notificationStyles.nofificatons)}>
             <button
               style={{
                 color: "#3a3a3a",
@@ -46,49 +46,63 @@ class Notifications extends Component {
                 console.log("Close button has been clicked");
               }}
             >
-              <img src={closeIcon} alt="close icon" width="10px" />
+              <img src={closeIcon} alt="close icon" width="15px" />
             </button>
-            {this.props.listNotifications.length != 0 ? <p>Here is the list of notifications</p> : null}
+            {this.props.listNotifications.length != 0 ? (
+              <p>Here is the list of notifications</p>
+            ) : null}
             <ul>
-              {this.props.listNotifications.length == 0 ? <NotificationItem type="default" value="No new notification for now" /> : null}
+              {this.props.listNotifications.length == 0 ? (
+                <NotificationItem
+                  type="default"
+                  value="No new notification for now"
+                />
+              ) : null}
               {this.props.listNotifications.map((val, idx) => {
-                return <NotificationItem type={val.type} value={val.value} html={val.html} key={val.id} markAsRead={this.markAsRead} id={val.id} />;
+                return (
+                  <NotificationItem
+                    type={val.type}
+                    value={val.value}
+                    html={val.html}
+                    key={val.id}
+                    markAsRead={this.markAsRead}
+                    id={val.id}
+                  />
+                );
               })}
             </ul>
           </div>
         ) : null}
-      </React.Fragment>
+      </>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  Notifications: {
-    padding: "1em",
-    border: "2px dashed red",
+const notificationStyles = StyleSheet.create({
+  nofificatons: {
+    border: "3px dotted #e1484c",
+    padding: "6px 12px",
     position: "absolute",
-    top: "1.8em",
-    right: "0",
+    top: "21px",
+    right: "7px",
+    marginTop: "12px",
+    zIndex: 100,
   },
-
-  "notification-header": {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-
   menuItem: {
-    textAlign: "right",
+    position: "absolute",
+    top: "7px",
+    right: "7px",
   },
 });
-
-Notifications.propTypes = {
-  displayDrawer: PropTypes.bool,
-  listNotifications: PropTypes.arrayOf(NotificationItemShape),
-};
 
 Notifications.defaultProps = {
   displayDrawer: false,
   listNotifications: [],
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropeTypes.bool,
+  listNotifications: PropeTypes.arrayOf(NotificationItemShape),
 };
 
 export default Notifications;
